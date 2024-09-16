407c76957c945a4659fcda94dc27c0382e4149e6
filip <fpolski22@gmail.com>
2024-09-16 08:30:23 +0200

    poprawa

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,17 @@ public class Main {
         System.out.println("Wprowadz liczbe punktow");
         int points = scan.nextInt();
 
-        if(points < 50) {
+        if(points >=0 && points < 50) {
             System.out.println("Musisz się jescze dużo nauczyc.");
         }
         else if(points >= 50 && points <= 70) {
             System.out.println("Średni wynik, ale jesteś na dobrej drodze.");
         }
-        else if(points > 70) {
+        else if(points > 70 && points <=100) {
             System.out.println("Świetnie! Dobrze Ci poszło.");
+        }
+        else {
+            System.out.println("Zla liczba punktow");
         }
     }
 }
